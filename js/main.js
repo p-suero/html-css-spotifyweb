1a03a1c582b6125f93d5b20788b4905c12239dac
Paolo <paolo.suero@gmail.com>
2020-08-20 19:21:43 +0200

    Aggiunto popup

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//rimuovo il popup al click del messaggio di chiusura
+document.getElementById("remove-popup").addEventListener("click", function() {
+  document.getElementById("wrapper-popup").remove()
+});
